80a62e63957da0df0cd4a150a539dec71f1c7332
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-01-31 12:18:19 -0800

    function written for components SkylineDropdown, WaterfrontDropdown, CastleDropdown, called in App. Functions render City img element src based on select change value

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   // you'll need to track a city name, which starts as the city name of your choice.
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
   const [skylineId, setSkylineId] = useState(1);
-  const [waterfrontId, setWaterFrontId] = useState(1);
+  const [waterfrontId, setWaterfrontId] = useState(1);
   const [castleId, setCastleId] = useState(1);
   const [cityName, setCityName] = useState('Portland');
   const [citySlogans, setCitySlogans] = useState([]);
@@ -38,6 +38,9 @@ function App() {
           
           In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state 
           */}
+          <SkylineDropdown setSkylineId={setSkylineId} />
+          <WaterfrontDropdown setWaterfrontId={setWaterfrontId} />
+          <CastleDropdown setCastleId={setCastleId} />
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
         {/* here, the SloganList component takes the array of slogans that lives in state */}
