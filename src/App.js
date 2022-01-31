2ecff8ad0a940d2239cf886f6b295f98882a4a22
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-01-31 12:01:40 -0800

    render image element in function City based on skylineId, waterfrontId, castleId

--- src/App.js
+++ src/App.js
@@ -13,13 +13,21 @@ function App() {
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
   // you'll need to track a city name, which starts as the city name of your choice.
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
+  const [skylineId, setSkylineId] = useState(1);
+  const [waterfrontId, setWaterFrontId] = useState(1);
+  const [castleId, setCastleId] = useState(1);
+  const [cityName, setCityName] = useState('Portland');
+  const [citySlogans, setCitySlogans] = useState([]);
 
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      <City skylineId={skylineId}
+        waterfrontId={waterfrontId}
+        castleId={castleId} />
       <h1>
         {/* dynamically update the city name here using state */}
-        Welcome to beautiful Portland!
+        Welcome to beautiful {cityName}!
       </h1>
       <div className='bottom'>
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
--- src/City.js
+++ src/City.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export default function City(props) {
+export default function City({ skylineId,
+  waterfrontId,
+  castleId }) {
+
   return (
     <div className="city">
-      {/* this component should take 3 props: waterfrontId, skylineId, and castlId */}
-      {/* use these 3 ids in img tags load approriate images that live in the public directory */}
+      {/* this component should take 3 props: waterfrontId, skylineId, and castleId */}
+      {/* use these 3 ids in img tags load appropriate images that live in the public directory */}
+      <img src={`skyline-${skylineId}.jpeg`} alt="" />
+      <img src={`waterfront-${waterfrontId}.jpeg`} alt="" />
+      <img src={`castle-${castleId}.jpeg`} alt="" />
     </div>
   );
 }
